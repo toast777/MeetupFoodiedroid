07d0ed91137aa4d5c29ff362dec7f117388d0748
Chuck Robinson <gdgkctest@users.noreply.github.com>
2018-09-30 21:39:48 -0500

    Added a Firebase Recyclerview to FirebaseActivity 2

---
+++ app/src/main/java/com/chuck/android/meetupfoodiedroid/adapters/FirebaseFoodAdapter.java
@@ -0,0 +1,76 @@
+package com.chuck.android.meetupfoodiedroid.adapters;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.chuck.android.meetupfoodiedroid.OrderStartActivity;
+import com.chuck.android.meetupfoodiedroid.R;
+import com.chuck.android.meetupfoodiedroid.models.FirebaseFoodItem;
+
+import java.util.List;
+
+public class FirebaseFoodAdapter extends RecyclerView.Adapter<FirebaseFoodAdapter.FoodItemViewHolder>{
+
+    private List<FirebaseFoodItem> foodList;
+
+    public class FoodItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ConstraintLayout foodLayout;
+        TextView foodName;
+        TextView foodPrice;
+        FoodItemViewHolder(View v) {
+            super(v);
+            //Define Viewholder Text objects
+            foodLayout = v.findViewById(R.id.rv_food_items);
+            foodName = v.findViewById(R.id.rvtv_food_name);
+            foodPrice = v.findViewById(R.id.rvtv_food_price);
+            v.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            Intent myIntent = new Intent(view.getContext(), OrderStartActivity.class);
+            myIntent.putExtra("EXTRA_FOOD_NAME", foodList.get(position).getName());
+            myIntent.putExtra("EXTRA_FOOD_IMGSRC", foodList.get(position).getImgSrc());
+            myIntent.putExtra("EXTRA_FOOD_NUMBERADDONS", foodList.get(position).getNumAddOns());
+            myIntent.putExtra("EXTRA_FOOD_PRICE", foodList.get(position).getPrice());
+            myIntent.putExtra("EXTRA_FOOD_ALLOWADDONS"foodList.get(position).isAllowAddOns());
+            view.getContext().startActivity(myIntent);
+
+
+        }
+    }
+    @NonNull
+    @Override
+    public FirebaseFoodAdapter.FoodItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.food_item_list,parent,false);
+        return new FoodItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FirebaseFoodAdapter.FoodItemViewHolder holder, int position) {
+        if (foodList != null)
+        {
+            holder.foodName.setText(foodList.get(position).getName());
+            holder.foodPrice.setText(Double.toString(foodList.get(position).getPrice()));
+            //What do we populate textview with
+        }
+    }
+    public void setFoodList(List<FirebaseFoodItem> currentFoodList) {
+        this.foodList = currentFoodList;
+        notifyDataSetChanged();
+    }
+    @Override
+    public int getItemCount() {
+        if (foodList != null)
+            return foodList.size();
+        else
+            return 0;
+    }
+}
---
+++ app/src/main/java/com/chuck/android/meetupfoodiedroid/models/FirebaseFoodItem.java
@@ -0,0 +1,63 @@
+package com.chuck.android.meetupfoodiedroid.models;
+
+public class FirebaseFoodItem {
+    private String name;
+    private String imgSrc;
+    private double price;
+    private boolean allowAddOns;
+    private int numAddOns;
+
+
+    public FirebaseFoodItem( String name, String imgSrc, double price,boolean allowAddOns,int numAddOns) {
+        this.name = name;
+        this.imgSrc = imgSrc;
+        this.price = price;
+        this.allowAddOns = allowAddOns;
+        this.numAddOns = numAddOns;
+    }
+    public FirebaseFoodItem(){
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAllowAddOns() {
+        return allowAddOns;
+    }
+
+    public void setAllowAddOns(boolean allowAddOns) {
+        this.allowAddOns = allowAddOns;
+    }
+
+    public int getNumAddOns() {
+        return numAddOns;
+    }
+
+    public void setNumAddOns(int numAddOns) {
+        this.numAddOns = numAddOns;
+    }
+}
